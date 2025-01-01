92b812bb5bd916e8804bebf2830e6d15931be590
SamKhan32 <106261217+SamKhan32@users.noreply.github.com>
2024-12-31 19:58:49 -0500

    Delete cigarParty directory

--- cigarParty/Cigar.py
+++
@@ -1,8 +0,0 @@
-class Cigar(object):
-    """A cigar is an object for use in cigar party"""
-
-    def ___init___(self, quality):
-        self.quality = quality
-    def ___str___(self):
-        return("This Cigar is of ", self.quality, " quality")
-    
